fe3cae0b528c3077b4b714062f855e45539c064b
Sonja Friedl <72401504+Soleviso@users.noreply.github.com>
2020-11-09 14:03:30 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,11 @@
+x = int(input("Eingabe Zahl x: "))
+y = int(input("Eingabe Zahl y: "))
+operation = input("Rechenaufgabe (+, -, /, *): ")
+if operation == "+":
+    print(x + y)
+if operation == "-":
+    print(x - y)
+if operation == "/":
+    print(x / y)
+if operation == "*":
+    print(x * y)
